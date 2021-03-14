8dc42076e8e73b25afb9fb84e5c7f3c2d4371836
Kamenev Nikolay <79969997909@mail.ru>
2021-03-14 13:59:10 +0300

    add page Auth

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,39 @@
 import {Component} from 'react';
+import s from './Auth.module.css';
+import Button from "../../components/UI/Button/Button";
+import Input from "../../components/UI/Input/Input";
 
 export default class Auth extends Component {
+  loginHandler = () => {
+
+  }
+
+  registerHandler = () => {
+
+  }
+
+  submitHandler = e => {
+    e.preventDefault()
+  }
+
   render() {
     return (
-      <div>
-        <h1>Auth</h1>
+      <div className={s.Auth}>
+        <div>
+          <h1>Авторизация</h1>
+
+          <form onSubmit={this.submitHandler} className={s.AuthForm}>
+            <Input
+              label='Email'
+            />
+            <Input
+              label='Пароль'
+              errorMessage={'test'}
+            />
+            <Button type='success' onClick={this.loginHandler}>Войти</Button>
+            <Button type='primary' onClick={this.registerHandler}>Зарегистрироваться</Button>
+          </form>
+        </div>
       </div>
     )
   }
